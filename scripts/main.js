e7477f43be0a33630b0f2b959a577bf741d99499
s3kken <your.unicorn0@gmail.com>
2023-01-30 10:40:49 +0700

    3

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,8 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        description: "A pair of warm, fuzzy socks",
-        image: "./assets/vmSocks-blue-onWhite.jpg",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        altText: "A pair of socks"
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        inStock: true,
     }
 })
