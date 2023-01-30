0f6b85bc9e46c8b24dba9a31dd3f3bef78745e48
s3kken <your.unicorn0@gmail.com>
2023-01-30 11:25:16 +0700

    6.1

--- scripts/main.js
+++ scripts/main.js
@@ -9,13 +9,21 @@ let app = new Vue({
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
             },
             {
                 variantId: 2235,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
         ],
         cart: 0,
+    },
+
+    methods: {
+        addToCart() {
+            this.cart += 1
+        }
     }
 })
